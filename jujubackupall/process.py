fa2c6810ccf1beb7839eba2f6f1e491eb837e1e5
Zachary Zehring <zachary.zehring@canonical.com>
2021-08-04 14:07:04 -0400

    Remove unused import.

--- jujubackupall/process.py
+++ jujubackupall/process.py
@@ -17,7 +17,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 """Module that processes the desired backups."""
-from contextlib import contextmanager
 from pathlib import Path
 import logging
 from typing import List, NamedTuple
