586872cc65d9f031c95028269ac5d1c2f83ea92f
Zachary Zehring <zachary.zehring@canonical.com>
2021-08-13 16:49:54 -0400

    Add versioning and package details to setup.py.

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
+    name="juju-backup-all",
+    author="Canonical",
+    description="Automatic discovery backup tool for charms, controllers, and configs",
+    url="https://launchpad.net/juju-backup-all",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
     packages=find_packages(),
     entry_points={
         'console_scripts': ['juju-backup-all = jujubackupall.cli:main']
