33ca962a8865f0e270c2c1c4220dde573423aa68
Chi Wai Chan <chiwai.chan@canonical.com>
2025-05-09 09:57:43 +0800

    Move tmp dir to home dir.

--- tests/functional/test_backup.py
+++ tests/functional/test_backup.py
@@ -16,12 +16,21 @@
 
 import glob
 import json
+import shutil
 import subprocess
 from pathlib import Path
 
 import pytest
 
 WAIT_TIMEOUT = 20 * 60
+
+
+@pytest.fixture()
+def tmp_dir():
+    d = Path().home() / "tmp-jba"
+    d.mkdir()
+    yield d
+    shutil.rmtree(d)
 
 
 @pytest.mark.abort_on_fail
@@ -54,20 +63,20 @@ async def test_build_and_deploy(ops_test):
 
 
 @pytest.mark.parametrize("backup_location", ["/var/backups/mysql", "/home/ubuntu/abc"])
-def test_mysql_innodb_backup(backup_location, ops_test, tmp_path: Path):
+def test_mysql_innodb_backup(backup_location, ops_test, tmp_dir: Path):
     mysql_innodb_app_name = "mysql"
     model_name = ops_test.model.name
     controller_name = ops_test.controller_name
     mysql_innodb_app = ops_test.model.applications.get(mysql_innodb_app_name)
 
     output = subprocess.check_output(
-        f"juju-backup-all -o {tmp_path} -e etcd -e postgresql -x -j --backup-location-on-mysql {backup_location}",
+        f"juju-backup-all -o {tmp_dir} -e etcd -e postgresql -x -j --backup-location-on-mysql {backup_location}",
         shell=True,
     )
     output_dict = json.loads(output)
-    expected_output_dir = tmp_path / controller_name / model_name / mysql_innodb_app_name
+    expected_output_dir = tmp_dir / controller_name / model_name / mysql_innodb_app_name
     app_backup_entry = output_dict.get("app_backups")[0]
-    assert any(str(tmp_path) in x.get("download_path") for x in output_dict.get("app_backups"))
+    assert any(str(tmp_dir) in x.get("download_path") for x in output_dict.get("app_backups"))
     assert app_backup_entry.get("controller") == controller_name
     assert any(x.get("model") == model_name for x in output_dict.get("app_backups"))
     assert app_backup_entry.get("charm") in mysql_innodb_app.data.get("charm-url")
@@ -76,20 +85,20 @@ def test_mysql_innodb_backup(backup_location, ops_test, tmp_path: Path):
 
 
 @pytest.mark.parametrize("backup_location", ["/home/ubuntu", "/home/ubuntu/abc"])
-def test_postgresql_backup(backup_location, ops_test, tmp_path: Path):
+def test_postgresql_backup(backup_location, ops_test, tmp_dir: Path):
     postgresql_app_name = "postgresql"
     model_name = ops_test.model.name
     controller_name = ops_test.controller_name
     postgresql_app = ops_test.model.applications.get(postgresql_app_name)
 
     output = subprocess.check_output(
-        f"juju-backup-all -o {tmp_path} -e etcd -e mysql-innodb-cluster -x -j --backup-location-on-postgresql {backup_location}",
+        f"juju-backup-all -o {tmp_dir} -e etcd -e mysql-innodb-cluster -x -j --backup-location-on-postgresql {backup_location}",
         shell=True,
     )
     output_dict = json.loads(output)
-    expected_output_dir = tmp_path / controller_name / model_name / postgresql_app_name
+    expected_output_dir = tmp_dir / controller_name / model_name / postgresql_app_name
     app_backup_entry = output_dict.get("app_backups")[0]
-    assert any(str(tmp_path) in x.get("download_path") for x in output_dict.get("app_backups"))
+    assert any(str(tmp_dir) in x.get("download_path") for x in output_dict.get("app_backups"))
     assert app_backup_entry.get("controller") == controller_name
     assert any(x.get("model") == model_name for x in output_dict.get("app_backups"))
     assert app_backup_entry.get("charm") in postgresql_app.data.get("charm-url")
@@ -98,19 +107,19 @@ def test_postgresql_backup(backup_location, ops_test, tmp_path: Path):
 
 
 @pytest.mark.parametrize("backup_location", ["/home/ubuntu/etcd-snapshots", "/home/ubuntu/abc"])
-def test_etcd_backup(backup_location, ops_test, tmp_path: Path):
+def test_etcd_backup(backup_location, ops_test, tmp_dir: Path):
     etcd_app_name = "etcd"
     model_name = ops_test.model.name
     controller_name = ops_test.controller_name
     etcd_app = ops_test.model.applications.get(etcd_app_name)
     output = subprocess.check_output(
-        f"juju-backup-all -o {tmp_path} -e mysql-innodb-cluster -e postgresql -x -j --backup-location-on-etcd {backup_location}",
+        f"juju-backup-all -o {tmp_dir} -e mysql-innodb-cluster -e postgresql -x -j --backup-location-on-etcd {backup_location}",
         shell=True,
     )
     output_dict = json.loads(output)
-    expected_output_dir = tmp_path / controller_name / model_name / etcd_app_name
+    expected_output_dir = tmp_dir / controller_name / model_name / etcd_app_name
     app_backup_entry = output_dict.get("app_backups")[0]
-    assert any(str(tmp_path) in x.get("download_path") for x in output_dict.get("app_backups"))
+    assert any(str(tmp_dir) in x.get("download_path") for x in output_dict.get("app_backups"))
     assert app_backup_entry.get("controller") == controller_name
     assert any(x.get("model") == model_name for x in output_dict.get("app_backups"))
     assert app_backup_entry.get("charm") in etcd_app.data.get("charm-url")
@@ -118,30 +127,30 @@ def test_etcd_backup(backup_location, ops_test, tmp_path: Path):
     assert glob.glob(str(expected_output_dir) + "/etcd-snapshot*.gz")
 
 
-def test_juju_controller_backup(ops_test, tmp_path: Path):
+def test_juju_controller_backup(ops_test, tmp_dir: Path):
     controller_name = ops_test.controller_name
     output = subprocess.check_output(
-        f"juju-backup-all -o {tmp_path} -e etcd -e mysql-innodb-cluster -e postgresql -j",
+        f"juju-backup-all -o {tmp_dir} -e etcd -e mysql-innodb-cluster -e postgresql -j",
         shell=True,
     )
     output_dict = json.loads(output)
-    expected_output_dir = tmp_path / controller_name
+    expected_output_dir = tmp_dir / controller_name
     controller_backup_entry = output_dict.get("controller_backups")[0]
-    assert str(tmp_path) in controller_backup_entry.get("download_path")
+    assert str(tmp_dir) in controller_backup_entry.get("download_path")
     assert controller_backup_entry.get("controller") == controller_name
     assert expected_output_dir.exists()
     assert glob.glob(str(expected_output_dir) + "/juju-controller-backup*.gz")
 
 
-def test_juju_client_config_backup(tmp_path: Path):
+def test_juju_client_config_backup(tmp_dir: Path):
     output = subprocess.check_output(
-        f"juju-backup-all -o {tmp_path} -e etcd -e mysql-innodb-cluster -e postgresql -x",
+        f"juju-backup-all -o {tmp_dir} -e etcd -e mysql-innodb-cluster -e postgresql -x",
         shell=True,
     )
     output_dict = json.loads(output)
-    expected_output_dir = tmp_path / "local_configs"
+    expected_output_dir = tmp_dir / "local_configs"
     config_backup_entry = output_dict.get("config_backups")[0]
-    assert str(tmp_path) in config_backup_entry.get("download_path")
+    assert str(tmp_dir) in config_backup_entry.get("download_path")
     assert config_backup_entry.get("config") == "juju"
     assert expected_output_dir.exists()
     assert glob.glob(str(expected_output_dir) + "/juju-*.gz")
